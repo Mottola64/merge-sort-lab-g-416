7547a4c013b978184b0a26aee4b0b8a086b09093
Michael Mottola <mottola64@gmail.com>
2020-09-23 22:03:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ function merge(arrayOne, arrayTwo) {
   let sorted = []
   while(arrayOne.length !=0 && arrayTwo.length !=0) {
     if (arrayOne[0] < arrayTwo[0]) {
-      findMinAndRemoveSorted(arrayOne)
+      sorted.push(findMinAndRemoveSorted(arrayOne))
     }
     else {
-      findMinAndRemoveSorted(arrayTwo)
+      sorted.push(findMinAndRemoveSorted(arrayTwo))
     }
   }
+  return sorted.concat(arrayOne).concat(arrayTwo)
 }
