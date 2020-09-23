99b63ce07503a692d8e7b6ded4deea04a3d53563
Michael Mottola <mottola64@gmail.com>
2020-09-23 22:07:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,9 @@ function merge(arrayOne, arrayTwo) {
   return sorted.concat(arrayOne).concat(arrayTwo)
 }
 
-function mergeSort()
+function mergeSort(array){
+  let midpoint = array.length/2
+  let firstHalf = array.slice(0, midpoint)
+  let secondHalf = array.slice(midpoint, array.length)
+  return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+}
