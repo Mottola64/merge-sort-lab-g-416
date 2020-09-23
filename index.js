1f6e677aa8697aa3bd616103dbd19861c83b7aff
Michael Mottola <mottola64@gmail.com>
2020-09-23 22:00:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function findMinAndRemoveSorted(array) {
   return array.shift()
 }
 
+function merge(arrayOne, arrayTwo) {
+  let sorted = []
+  while(arrayOne.length !=0 && arrayTwo.length !=0) {
+    let currentMin = findMinAndRemoveSorted(arrayOne)
+  }
+}
